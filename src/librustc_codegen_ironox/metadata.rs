7620cf2e1bebd5b84b343b0f6fde58a6727a7bc3
Gabriela Alexandra Moldovan <gabi_250@live.com>
2018-10-17 22:43:53 +0100

    Read rlib metadata with `ar`.

--- src/librustc_codegen_ironox/metadata.rs
+++ src/librustc_codegen_ironox/metadata.rs
@@ -15,7 +15,20 @@ pub struct IronOxMetadataLoader;
 impl MetadataLoader for IronOxMetadataLoader {
     fn get_rlib_metadata(&self, _: &Target, filename: &Path)
         -> Result<MetadataRef, String> {
-        unimplemented!("get_rlib_metadata {:?}", filename);
+        let input_file =
+            File::open(filename)
+                .map_err(|_e| format!("failed to read {}", filename.display()))?;
+        let mut archive = ar::Archive::new(input_file);
+        let mut buf = vec![];
+        while let Some(entry) = archive.next_entry() {
+            let mut entry = entry.expect("failed to parse archive entry");
+            if entry.header().identifier() == METADATA_FILENAME.to_string().as_bytes() {
+                entry.read_to_end(&mut buf).expect("failed to read metadata file");
+            }
+        }
+        let buf = OwningRef::new(box buf);
+        let buf: OwningRef<_, [u8]> = buf.map(|v| &v[..]);
+        Ok(rustc_erase_owner!(buf))
     }
 
     fn get_dylib_metadata(&self, _target: &Target, filename: &Path)
