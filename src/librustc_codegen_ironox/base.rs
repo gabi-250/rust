63733816b6b6384ca2dc01dfc0ee200658753bfd
Gabriela Alexandra Moldovan <gabi_250@live.com>
2018-10-26 02:51:47 +0100

    Refactor the backend.

--- src/librustc_codegen_ironox/base.rs
+++ src/librustc_codegen_ironox/base.rs
@@ -1,23 +1,9 @@
-use rustc::middle::cstore::EncodedMetadata;
-use rustc::session::config::OutputFilenames;
 use rustc::ty::TyCtxt;
-use rustc_data_structures::svh::Svh;
-use syntax_pos::symbol::Symbol;
+use syntax_pos::symbol::InternedString;
+use rustc::mir::mono::Stats;
 
-use std::any::Any;
-use std::sync::Arc;
-use std::sync::mpsc;
-
-#[allow(dead_code)]
-pub struct OngoingCodegen {
-    crate_name: Symbol,
-    crate_hash: Svh,
-    metadata: EncodedMetadata,
-    output_filenames: Arc<OutputFilenames>,
-}
-
-pub fn codegen_crate<'a, 'tcx>(_tcx: TyCtxt<'a, 'tcx, 'tcx>,
-                               _rx: mpsc::Receiver<Box<dyn Any + Send>>)
-                               -> OngoingCodegen {
-    unimplemented!("base::codegen_crate");
+pub fn compile_codegen_unit<'ll, 'tcx>(_tcx: TyCtxt<'ll, 'tcx, 'tcx>,
+                                  _cgu_name: InternedString)
+                                  -> Stats {
+    unimplemented!("compile_codegen_unit");
 }
