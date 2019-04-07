52fd0142a567592ecd789c046d805a1613f786b0
Gabriela Alexandra Moldovan <gabi_250@live.com>
2019-04-07 02:15:47 +0100

    Add the IronOx build script.

--- build_ironox.sh
+++ build_ironox.sh
@@ -11,4 +11,3 @@ RUST_BACKTRACE=1 ./x.py build --config .buildbot-ironox.toml --stage 1
 # Copy the libs compiled by the LLVM backend:
 cp -r ./llvm_build/x86_64-unknown-linux-gnu/stage1/lib/rustlib/x86_64-unknown-linux-gnu/lib \
 ./build/x86_64-unknown-linux-gnu/stage1/lib/rustlib/x86_64-unknown-linux-gnu/
-
